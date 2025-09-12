cde6cc937563babec1781996064c57a6e5c8379d
winston <1473457657@qq.com>
2025-09-12 10:57:06 +0800

    哈希表查找

--- leecode-01/src/test/java/com/itheima/Solution.java
+++ leecode-01/src/test/java/com/itheima/Solution.java
@@ -1,18 +1,23 @@
 package com.itheima;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // new一个哈希表，键是数组的值，值是数组的索引（拿来返回满足条件的）
+        Map <Integer, Integer> map = new HashMap<>();
         // 遍历数组
         for (int i = 0; i < nums.length; i++) {
-            // 然后再遍历内层数组
-            for (int j = i + 1; j < nums.length; j++) {
-                // 如果满足条件的就返回
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+            // 得到需要的数字
+            int need = target - nums[i];
+            // 如果哈希表中存在就直接返回
+            if (map.containsKey(need)) {
+                return new int[]{map.get(need), i};
             }
+            // 没有就存储
+            map.put(nums[i], i);
         }
-        // 不满足条件的就返回一个随机，不太可能出现
         return new int[0];
     }
 }
