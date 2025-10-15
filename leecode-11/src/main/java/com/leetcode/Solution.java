840e5c9d1e4c4ebc08e5008fbfbfb2b7e6d76f1f
winston <1473457657@qq.com>
2025-10-15 09:02:05 +0800

    盛水最多的容器

---
+++ leecode-11/src/main/java/com/leetcode/Solution.java
@@ -0,0 +1,18 @@
+package com.leetcode;
+
+public class Solution {
+    public int MaxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int ans = 0;
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            ans = Math.max(ans, area);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return ans;
+    }
+}
