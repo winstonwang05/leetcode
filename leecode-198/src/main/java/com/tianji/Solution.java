9358b104dd85a1f4fbdb549213054f76829718c5
winston <1473457657@qq.com>
2025-10-17 10:22:08 +0800

    打家劫舍

---
+++ leecode-198/src/main/java/com/tianji/Solution.java
@@ -0,0 +1,20 @@
+package com.tianji;
+
+public class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        if (n == 2) return Math.max(nums[0], nums[1]);
+
+        // 初始化dp
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        // 状态转移
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[n - 1];
+    }
+}
